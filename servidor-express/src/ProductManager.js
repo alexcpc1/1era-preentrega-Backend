96f9a4fdcca5658ca57ae4a83bfc57d1af1e9985
aljornacpc1 <aljornacpc1@gmail.com>
2023-04-23 18:27:10 -0400

    actualizacion 4

--- servidor-express/src/ProductManager.js
+++ servidor-express/src/ProductManager.js
@@ -1,9 +1,9 @@
 
-const fs = require("fs");
+import fs from "fs";
 
-export class ProductManager {
+class ProductManager {
     constructor(pathName){
-        this.path=pathName;
+        this.path = pathName;
     }
     fileExists(){
         return fs.existsSync(this.path);
@@ -12,23 +12,41 @@ export class ProductManager {
     generateId(products){
         let newId;
         if(!products.length){
-            newId=1;
+            newId = 1;
         } else{
-            newId = products[products.length-1].id+1;
+            newId = products[products.length - 1].id + 1;
         }
         return newId;
     }
 
-    async addProduct (product){
+    async addProduct (title, descripcion, price, thumbnail, code, stock){
+        
+        if(!title || !descripcion || !price || !thumbnail|| !code || !stock){
+         return console.log("Todos los campos deben ser Obligatorios");
+        }
         try{
             if(this.fileExists()){
                 const content = await fs.promises.readFile(this.path, "utf-8");
-                const products =JSON.parse(content);
+                const products = JSON.parse(content);
+                // Hice un filter para saber si ya existe el code, si es 0 no existe, si esta mayor a 0 si existe
+                if (products.filter(obj => obj.code == code).length < 1) { 
                 const productId = this.generateId(products);
+                var product = new Object();   
+                product.title = title;
+                product.descripcion = descripcion;
+                product.price = price;
+                product.thumbnail = thumbnail;
+                product.code = code;
+                product.stock = stock;             
                 product.id = productId;
                 products.push(product);
+                //Actualiza el archivo de los productos          
                 await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
                 return product;
+               } else {
+                //En caso de que exista lanza el log y no agrega el producto ni actualiza el archivo de productos!
+                return console.log("Ya existe un producto con este Codigo");
+               }                
             } else {
                 const productId = this.generateId([]);
                 product.id = productId;
@@ -124,32 +142,32 @@ export class ProductManager {
         }
     };
 }
+export {ProductManager}
+// const manager = new ProductManager("./products.json");
 
-const manager = new ProductManager("./products.json");
-
-const functionPrincipal = async()=>{
-    try{
-        // const productAdded1 = await manager.addProduct({title: "Cereal", descripcion:   "Cereal Infantil Nestum Frutilla - 250GR", price: 6000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20386774-qf9cW0sV-medium.png",  code: "1012", stock: 100});
-        // const productAdded2 = await manager.addProduct({title: "Mantequilla", descripcion:   "Mantequilla con Sal - 250 GR", price: 1500, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/4011088-aosxGOr0-medium.jpg",  code: "1014", stock: 20});
-        // const productAdded3 = await manager.addProduct({title: "Chorizo", descripcion:  "Chorizo Parrillero - 20 Und", price: 2000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20050239-Oy6zFS-p-medium.jpg",  code: "1016", stock: 30});
-        // const productAdded4 = await manager.addProduct({title: "Queso", descripcion:  "Queso Gauda Laminado - 500 GR", price: 4000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20211526-38YL_0zf-medium.jpg",  code: "1012", stock: 300});
-        // const productAdded5 = await manager.addProduct({title: "Risotto", descripcion: "Risotto de Champiñones 40Gr", price: 3000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20548165-LhlJB88C-medium.jpg", code: "1010", stock: 40});
-        // const productAdded6 = await manager.addProduct({title: "Pasta", descripcion: "pasta larga 200Gr", price: 1000, thumbnail: "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20548165-LhlJB88C-medium.jpg", code: "1033", stock: 10});
-        // console.log("productAdded1; ", productAdded1);
-        const product1 = await manager.getProductById(1);
-        // console.log("product1: ", product1);
-        const product2 = await manager.getProductById(3);
-        // console.log("product2: ", product2);
+// const functionPrincipal = async()=>{
+//     try{
+        // const productAdded1 = await manager.addProduct("Cereal", "Cereal Infantil Nestum Frutilla - 250GR", 6000, "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20386774-qf9cW0sV-medium.png", "1012", 100});
+        // const productAdded2 = await manager.addProduct("Mantequilla",  "Mantequilla con Sal - 250 GR", 1500, "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/4011088-aosxGOr0-medium.jpg", "1014", 20});
+        // const productAdded3 = await manager.addProduct("Chorizo", "Chorizo Parrillero - 20 Und",  2000, "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20050239-Oy6zFS-p-medium.jpg", "1016",  30});
+        // const productAdded4 = await manager.addProduct("Pizza", "Pizza de peperoni mediana", "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20211526-38YL_0zf-medium.jpg", "3333", 8800});
+        // const productAdded5 = await manager.addProduct("Yogurt", "Yogurt de durazno 100Gr", 9999, "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20050239-Oy6zFS-p-medium.jpg", "44444", 888); 
+        // producto imcompleto:
+        // const productAdded6 = await manager.addProduct("Pasta", "pasta larga 200Gr", 1000, "https://7483c243aa9da28f329c-903e05bc00667eb97d832a11f670edad.ssl.cf1.rackcdn.com/20548165-LhlJB88C-medium.jpg", 10);
+        // console.log("productAdded5; ", productAdded5);
+        // const product1 = await manager.getProductById(1);
+        // // console.log("product1: ", product1);
+        // const product2 = await manager.getProductById(3);
+        // // console.log("product2: ", product2);
         // const resultado = await manager.updateProduct(3,{price:4000});
-        // console.log("resultado: ", resultado);
-        const resultado2 = await manager.getProducts();
-        // console.log("resultado2: ", resultado2);
-        // const resultadodeleteProduct = await manager.deleteProduct(2);
-        // console.log("resultadodeleteProduct: ", resultadodeleteProduct);
+        // // console.log("resultado: ", resultado);
+        // const resultado2 = await manager.getProducts();
+        // // console.log("resultado2: ", resultado2);
+        // const productDelete = await manager.deleteProduct(8);
+        // console.log("productDelete: ", productDelete);
         
-    } catch(error){
-        console.log(error.message);
-    }
-    
-}
-functionPrincipal();
+//     } catch(error){
+//         console.log(error.message);
+//     }
+// }
+// functionPrincipal();
--- servidor-express/src/app.js
+++ servidor-express/src/app.js
@@ -1,16 +1,39 @@
-import express from "express";
-import { ProductManager } from "./ProductManager"; 
+import express, { request } from "express";
+import {ProductManager} from "./ProductManager.js";
 
 const app = express()
+app.use(express.urlencoded({ extended: true }));
+
+const productManager = new ProductManager("./src/products.json");
+// console.log(productManager);
 
 const port = 8080;
 
-const manager = new ProductManager();
+app.get("/products", async (req,res) => {
+    try{
+        let limit = parseInt (req.query.limit);
+        if(!limit) 
+            return  res.send(await productManager.getProducts())
+        const allProducts = await productManager.getProducts();
+        let productLimitet = allProducts.slice(0, limit)  
+        res.send(productLimitet);
+    } catch(error){
+        throw new error(error.mesage);
+    }
+});
+app.get("/products/:id", async (req,res) => {
+    try{
+        let id = parseInt (req.params.id);
+        const allProducts = await productManager.getProducts();
+        const productById = allProducts.find(product=>product.id===id);
+        res.send(productById);
+    } catch(error){
+        throw new error(error.mesage);
+    }
+});
 
-app.get("../products", async (req,res) => {
-    const price = await manager.getProducts();
-    resizeBy.send(price)
-})
-console.log(await price);
+app.listen(port,()=>
+    console.log(`Server listening on port ${port}`));
 
-app.listen(port,()=>console.log(`Server listening on port ${port}`));
+app.on("error", (error) => 
+    console.log(`Error del servidor ${error}`))
